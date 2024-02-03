7628f9352f3bd558c76a5960690e612ab11b6ba9
novesh <gautamnovesh@gmail.com>
2024-02-03 12:31:34 +0530

    some changes

--- src/App.js
+++ src/App.js
@@ -76,7 +76,7 @@ function App() {
                         <span className="measure">
                           {weatherData.main.humidity}%
                         </span>
-                        <span>humidity</span>
+                        <span>humid</span>
                       </div>
                     </div>
                   </>
